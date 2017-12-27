e1702eae0690e5421d4a398989544ef95b3c1786
Henry Chan <henry.chan@trefis.com>
2017-12-26 17:03:51 -0800

    checks if STORE_PASS is defined

--- signAPK.bat
+++ signAPK.bat
@@ -2,6 +2,7 @@
 @rem i.e. keytool -genkey -v -keystore yarntails.keystore -alias yarntails -keyalg RSA -keysize 2048 -validity 10000
 @rem remember to set the STORE_PASS
 @rem set STORE_PASS=xxxxxx
+if  not defined STORE_PASS (echo "STORE_PASS is not defined")
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore yarntails.keystore platforms\android\build\outputs\apk\android-release-unsigned.apk yarntails -storepass %STORE_PASS%
 del yarntails.apk
 %ANDROID_HOME%\build-tools\26.0.0\zipalign -v 4 platforms\android\build\outputs\apk\android-release-unsigned.apk yarntails.apk
