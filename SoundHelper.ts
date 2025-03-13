9c4ff1b5f5582bdd9cb8a539187bb75d2e007c63
Henry Chan <hchan@apache.org>
2025-03-12 23:16:54 -0700

    sound

--- SoundHelper.ts
+++ SoundHelper.ts
@@ -1,7 +1,8 @@
 import {Game} from './Game'
 import {Cell} from './Cell'
+import {ImageHelper} from './ImageHelper'
 export class SoundHelper {
-  static DIR : string = "snd";
+  static DIR : string = ImageHelper.BASE_PATH + "snd";
 
   populate() {
       Game.SOUND_LOCATIONS.push(SoundHelper.DIR + "/" + "meow.mp3");
