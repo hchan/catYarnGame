6ba9851b241184453236d41c96707cf09aca29d6
Henry Chan <hchan@apache.org>
2025-03-12 22:24:40 -0700

    Mark Mullen reported a bug in loading with mobile ... should be fixed now.  Thanks Mark

--- ImageHelper.ts
+++ ImageHelper.ts
@@ -1,8 +1,9 @@
 import {Game} from './Game'
 import {Cell} from './Cell'
 export class ImageHelper {
-  static MOBILE_DIR : string = "img";
-  static LARGE_DIR : string = "../catYarnGame/imgLarge";
+  static BASE_PATH : string = "../catYarnGame/";
+  static MOBILE_DIR : string = ImageHelper.BASE_PATH + "img";
+  static LARGE_DIR : string = ImageHelper.BASE_PATH + "imgLarge";
   populate() {
       Game.IMAGE_LOCATIONS.push(ImageHelper.getImageDir() + "/" + "cat0" + Cell.IMAGENAME_SUFFIX);
       Game.IMAGE_LOCATIONS.push(ImageHelper.getImageDir() + "/" + "cat1" + Cell.IMAGENAME_SUFFIX);
@@ -10,6 +11,13 @@ export class ImageHelper {
   }
 
   static getImageDir() : string {
+    var url = window.location.href;
+    if (url.indexOf("file://") != -1) {
+      ImageHelper.BASE_PATH = "";
+      this.MOBILE_DIR = ImageHelper.BASE_PATH + "img";
+      this.LARGE_DIR = ImageHelper.BASE_PATH + "imgLarge";
+    }
+    console.log(ImageHelper.LARGE_DIR);
     if (Game.MOBILE) {
       return ImageHelper.MOBILE_DIR;
     } else {
